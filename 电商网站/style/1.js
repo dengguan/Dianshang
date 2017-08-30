82db6720752f59bbbfee586e03af460f37696a5c
dengguan <1104625624@qq.com>
2017-08-30 17:48:59 +0800

    v1

---
+++ 电商网站/style/1.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function tiaozhuan(){
+	window.location.href="购物车.html"; 
+	}
+function denglu(){
+	window.location.href="F:\电商网站\style\登录.html" 
+	}
+function zhuce(){
+	window.location.href="注册.html"; 
+	}
